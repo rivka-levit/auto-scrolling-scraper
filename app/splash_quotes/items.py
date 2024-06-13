9f4cbbf13c16eb2046066ba68bb2bd804f57a418
rivka-yuliia <juliacreadora@gmail.com>
2024-06-13 15:29:33 +0300

    item model

--- app/splash_quotes/items.py
+++ app/splash_quotes/items.py
@@ -1,12 +1,32 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+"""
+Models for the scraped items.
+"""
 
 import scrapy
 
+from itemloaders.processors import Join  # noqa
+
+
+def clean(q):
+    """
+    Clean data. Get rid of extra quotes and special characters.
+
+    Args:
+        q (list): list of one string received form item loader.
+
+    Returns:
+        str: cleaned string.
+    """
+
+    return q[0].replace('\201c', '').replace('\u201d', '').replace('\u00e9', '')
+
 
 class SplashQuotesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    quote = scrapy.Field(
+        input_processor=clean,
+        output_processor=Join()
+    )
+    author = scrapy.Field(
+        input_processor=clean,
+        output_processor=Join()
+    )
