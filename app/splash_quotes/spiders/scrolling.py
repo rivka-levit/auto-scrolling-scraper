ea495e520b84456104e42c11bbf0cb018380b381
rivka-yuliia <juliacreadora@gmail.com>
2024-06-15 21:27:56 +0300

    lua script to auto scroll page

--- app/splash_quotes/spiders/scrolling.py
+++ app/splash_quotes/spiders/scrolling.py
@@ -11,13 +11,39 @@ class ScrollingSpider(scrapy.Spider):
     name = "scrolling"
     allowed_domains = ["quotes.toscrape.com", "172.24.0.2", "splash"]
     start_urls = ["https://quotes.toscrape.com/scroll"]
+    lua_script = """
+        function main(splash)
+            local num_scrolls = 20
+            local scroll_delay = 1
+        
+            local scroll_to = splash:jsfunc("window.scrollTo")
+            local get_body_height = splash:jsfunc(
+                "function() {return document.body.scrollHeight;}"
+            )
+            assert(splash:go(splash.args.url))
+            splash:wait(splash.args.wait)
+        
+            for _ = 1, num_scrolls do
+                local height = get_body_height()
+                for i = 1, 10 do
+                    scroll_to(0, height * i/10)
+                    splash:wait(scroll_delay/10)
+                end
+            end
+            return splash.html()
+        end
+    """
 
     def start_requests(self):
         for url in self.start_urls:
             yield SplashRequest(
                 url=url,
+                endpoint='execute',
                 callback=self.parse,
-                args={'wait': 1}
+                args={
+                    'wait': 1,
+                    'lua_source': self.lua_script
+                }
             )
 
     def parse(self, response, **kwargs):
